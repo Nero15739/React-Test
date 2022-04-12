2a02749327f08e1d796815030cc58dec7d8b499c
Nero15739 <andrewpajva@gmail.com>
2022-04-12 13:06:01 +1000

    Search function within the application

--- robo-friends/src/App.js
+++ robo-friends/src/App.js
@@ -1,24 +1,46 @@
-import React from "react";
+import React, {Component} from "react";
 import CardList from './cardList';
 import { robots } from "./robots";
 import SearchBox from './SearchBox';
 
-const state = {
-    robots:robots,
-    searchfeild:'';
-}
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            robots:robots,
+            searchField:''
+        }
 
+    }
 
+    //this is a event which changes teh displayed list of robots
+    //it's run when teh searchbox is changed.
+    onSearchChange = (event) => {
+        console.log(event.target.value);
+        //this sets teh state
+        this.setState({searchField: event.target.value})
+    }
 
-const App = () =>{
-    return(
-        <div className='tc'>
-            <h1 className="f1 tc">RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots} />
+    render(){
+        //this is careting a new var, then runing it through a function which is =>
+        //the function rerturns the robot if it includes the searchfield value. compared in lower case
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
             
-        </div>
-    );
+        })
+        console.log("renderupdate")
+        return(
+            <div className='tc'>
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots} />
+                
+            </div>
+        );
+    }
 }
+
+    
+
 
 export default App;
--- robo-friends/src/SearchBox.js
+++ robo-friends/src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({searchfield, searchChange}) => {
     return (
-        <div className='pa2'>
+        <div className='pa2' >
             <input
             className='pa3 bg-lightest-blue b--green ba br3'
             type='search'
             placeholder='search robots'
+            onChange={searchChange}
             />
         </div>
     )
